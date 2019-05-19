d9a8ce4d0671b0123a8d261c42285fe5f6aeba0f
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-05-19 16:23:20 +0400

    create UserList by using the useResources function reusing this function allowed us to write zero logic for the UserList Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
     // instead of state={resource:'posts'}
@@ -17,6 +18,7 @@ const App = () => {
 
   return (
     <div>
+        <UserList />
       <div>
         <button onClick={() => setResource('posts')}>
           Posts
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,19 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const useResource = resource => {
-  const [resources, setResources] = useState([]);
-
-  useEffect(() => {
-    (async resource => {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${resource}`
-      );
-      setResources(response.data);
-    })(resource);
-  }, [resource]);
-  return resources;
-};
+import React from "react";
+import useResource from './useResources';
 
 const ResourceList = ({ resource }) => {
   const resources = useResource(resource);
