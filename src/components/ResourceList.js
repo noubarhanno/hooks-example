fa2bc43fda61d8d144cbc9236a5ece24085b8956
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-05-19 09:01:20 +0400

    Updated the Resourcelist to fetch the data as per the state value by class based component

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,18 +1,33 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-class ResourceList extends React.Component{
-    state = { resources: [] };
+class ResourceList extends React.Component {
+  state = { resources: [] };
 
-    async componentDidMount(){
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+  // the reason that when we click on Todos button the component will be re rendered but componentDidMount lifecycle is going to be called in the second time
+  // if the component re-rendered as the component is mounted by a re-rendering is not meaning that the component has been re-mounted
+  // to solve this issue we need to use another lifecycle called componentDidUpdate
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
 
-        this.setState({ resources: response.data });
+    this.setState({ resources: response.data });
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+
+      this.setState({ resources: response.data });
     }
+  }
 
-    render(){
-        return <div>{this.state.resources.length}</div>;
-    }
+  render() {
+    return <div>{this.state.resources.length}</div>;
+  }
 }
 
 export default ResourceList;
