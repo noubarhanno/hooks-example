fceac8907da21ce960b22ab0aff91e4536c564ab
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-05-19 16:09:14 +0400

    We refactor the code to export all the login of hooks into one single function to be reused accross the app

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,50 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResource = resource => {
   const [resources, setResources] = useState([]);
-  // the reason that when we click on Todos button the component will be re rendered but componentDidMount lifecycle is going to be called in the second time
-  // if the component re-rendered as the component is mounted by a re-rendering is not meaning that the component has been re-mounted
-  // to solve this issue we need to use another lifecycle called componentDidUpdate
-  const fetchResource = async resource => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-
-    setResources(response.data);
-  };
-
-  // it's not allowed to use useEffect with sync function or a function with a promise like below 
-
-  // useEffect(async (resource) => {
-        // const response = await axios.get(`URL/${resource}`);
-  //})
-  // the above code is not allowed in useEffect
-  // instead we can do 
 
   useEffect(() => {
-      (async resource => {
-          const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-          setResources(response.data);
-      })(resource);
+    (async resource => {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      );
+      setResources(response.data);
+    })(resource);
   }, [resource]);
+  return resources;
+};
 
-  // the code above is the same of doing below 
-  // () => console.log('hi')
-  // the code above is only defining a function but not calling it in order to call it 
-  // const hi = () => console.log('hi')
-  // or
-  // (() => console.log('hi'))()
-  // and this is exactly what we did in the useEffect above
-
-//   useEffect(() => {
-//     fetchResource(resource);
-//   }, [resource]);
-
+const ResourceList = ({ resource }) => {
+  const resources = useResource(resource);
   return (
-  <ul>
-      {resources.map(record => <li key={record.id}>{record.title}</li>)}
-  </ul>
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
   );
 };
 
