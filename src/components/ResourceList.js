4ad3a0a0f5569ffa06b3c02bb628940a40d553ae
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-05-19 15:50:54 +0400

    Refactor the ResourceList to be function based component using useEffect and useState

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,33 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class ResourceList extends React.Component {
-  state = { resources: [] };
-
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
   // the reason that when we click on Todos button the component will be re rendered but componentDidMount lifecycle is going to be called in the second time
   // if the component re-rendered as the component is mounted by a re-rendering is not meaning that the component has been re-mounted
   // to solve this issue we need to use another lifecycle called componentDidUpdate
-  async componentDidMount() {
+  const fetchResource = async resource => {
     const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      `https://jsonplaceholder.typicode.com/${resource}`
     );
 
-    this.setState({ resources: response.data });
-  }
+    setResources(response.data);
+  };
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
+  // it's not allowed to use useEffect with sync function or a function with a promise like below 
 
-      this.setState({ resources: response.data });
-    }
-  }
+  // useEffect(async (resource) => {
+        // const response = await axios.get(`URL/${resource}`);
+  //})
+  // the above code is not allowed in useEffect
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
 
-  render() {
-    return <div>{this.state.resources.length}</div>;
-  }
-}
+  return <div>{resources.length}</div>;
+};
 
 export default ResourceList;
